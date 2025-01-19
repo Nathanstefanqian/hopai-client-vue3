3791350acc22260b183f559750ef4d592b40284b
nathanq <nathanstefanqian@gmail.com>
2025-01-19 23:22:31 +0800

    初始化这个项目,下一步清理一下冗余,然后建立基础页面和通信,以及建立token账号体系

---
+++ src/pages/home/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home h-648rpx  w-100vw relative">
+    <div class="home-header">
+      <Header />
+    </div>
+    <div class="home-main">
+      <Tab />
+      <Container />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Container from '@/components/home/Container.vue'
+import Header from '@/components/home/Header.vue'
+import Tab from '@/components/home/Tab.vue'
+</script>
+
+<style lang="scss" scoped>
+.home {
+  background-image: url('@/static/home/home-bg.svg');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  
+  &-main {
+    box-sizing: border-box;
+    position: absolute;
+    left:0;
+    top: 558rpx;
+    width: 100%;
+    padding: 0 32rpx;
+    border-radius: 48rpx 48rpx 0 0;
+    background-color: #f6f6f6;
+  }
+}
+</style>
